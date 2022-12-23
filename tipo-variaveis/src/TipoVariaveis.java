ee457b7d0cf6b4018b99cbb9041d6129460972a6
Velez <matheus.velez@gmail.com>
2022-12-23 11:35:59 -0300

    entendendo variaveis

--- tipo-variaveis/src/TipoVariaveis.java
+++ tipo-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,21 @@
 public class TipoVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String Meunome = "MATHEUS VELEZ";
+
+
+        double salarioMinimo =2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numerocurto2 = (short) numeroNormal;
+
+        int numero =5;
+
+        numero =10;
+
+
+
+        System.out.println(numero);
     }
 }
